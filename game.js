653e432c537869500913f2749c83e0b39b1a8947
Kamil K <kieltyka.kamil@gmail.com>
2023-05-19 07:39:31 +0200

    added files

---
+++ game.js
@@ -0,0 +1,37 @@
+const gameTiles = document.querySelectorAll(".tile");
+const gameBoard = document.querySelector("#game-board");
+const gameState = [
+  [gameTiles[0], gameTiles[1], gameTiles[2]],
+  [gameTiles[3], gameTiles[4], gameTiles[5]],
+  [gameTiles[6], gameTiles[7], gameTiles[8]],
+];
+gameBoard.addEventListener("click", (event) => {
+  const target = event.target;
+
+  let x, y;
+  gameState.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      if (column === target) {
+        x = rowIndex;
+        y = columnIndex;
+      }
+    });
+  });
+
+  let emptyX, emptyY;
+  gameState.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      if (column.innerText === "") {
+        emptyX = rowIndex;
+        emptyY = columnIndex;
+      }
+    });
+  });
+  console.log(emptyX, emptyY);
+  console.log(x, y);
+
+  const temp = gameState[x][y];
+  gameState[x][y] = gameState[emptyX][emptyY];
+  gameState[emptyX][emptyY] = temp;
+  console.log(gameState);
+});
